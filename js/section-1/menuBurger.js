ca6a4b7359819aeee5ddbb620becec47296ac76e
renatakov <renatakov1010@gmail.com>
2023-02-16 22:25:57 +0100

    section-1

--- js/section-1/menuBurger.js
+++ js/section-1/menuBurger.js
@@ -1,13 +1,31 @@
 const menuBtn = document.querySelector('.menu-btn');
+const navList = document.querySelector('.nav__list');
+const menu = document.querySelector('.menu');
+// const imgCall = document.querySelector('.imgCall');
+// const imgSearch = document.querySelector('.imgSearch');
+// const imgBuy = document.querySelector('.imgBuy');
+
+
 
 let menuOpen = false;
 menuBtn.addEventListener('click', ()=> {
     if(!menuOpen) {
         menuBtn.classList.add('open');
         menuOpen=true;
+        navList.style.display = 'flex';
+        menu.style.width = '500px';
+        menu.style.background = 'rgba(206, 195, 181, 0.2)'
+        menu.style.backdropFilter = 'blur(10px)';
+        navList.style.marginLeft = '100px';
+
     }
     else {
         menuBtn.classList.remove('open');
+        navList.style.display = 'none'
+        menu.style.width = '0';
+        menu.style.background = 'none'
+        menu.style.backdropFilter = 'none';
+
         menuOpen=false;
     }
 })
