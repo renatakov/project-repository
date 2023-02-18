b61b0a234479d5fc32e5dea548d18415ac25cef1
renatakov <renatakov1010@gmail.com>
2023-02-18 12:06:32 +0100

    menu-burger is done!

--- js/section-1/menuBurger.js
+++ js/section-1/menuBurger.js
@@ -7,39 +7,14 @@ const imgSearch1 = document.querySelector('.imgSearch');
 const imgBuy1 = document.querySelector('.imgBuy');
 const menuNav = document.querySelector('.menu-nav');
 
+const btnClose = document.querySelector('#close');
+
 
 let menuOpen = false;
 menuBtn.addEventListener('click', ()=> {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen=true;
-        navList.style.display = 'flex';
-        if(window.innerWidth >= 320 && window.innerWidth <= 426){
-            menu.style.marginLeft = '0';
-            menu.style.width = window.innerWidth / 2 + 'px';
-        }
-        imgBuy1.style.display = 'none';
-        imgCall1.style.display = 'none';
-        imgSearch1.style.display = 'none';
-        menuBtn.style.marginTop = '30px';
-        menu.style.paddingLeft = '20px';
-        menu.style.background = 'rgba(206, 195, 181, 0.2)'
-        menu.style.backdropFilter = 'blur(10px)';
-        menuNav.style.height = window.innerHeight * 1.76 + 'px';
-    }
-    else {
-        menuBtn.classList.remove('open');
-        navList.style.display = 'none'
-        menu.style.width = '0';
-        menu.style.marginLeft = '20px';
+    document.querySelector('.nav-m').style.zIndex = '1';
+})
 
-        menu.style.background = 'none'
-        menu.style.backdropFilter = 'none';
-        menuBtn.style.marginTop = '0';
-        menu.style.paddingLeft = '0';
-        imgBuy1.style.display = 'block';
-        imgCall1.style.display = 'block';
-        imgSearch1.style.display = 'block';
-        menuOpen=false;
-    }
+btnClose.addEventListener('click', ()=> {
+    document.querySelector('.nav-m').style.zIndex = '-1';
 })
