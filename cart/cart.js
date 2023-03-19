ae6980bfbec352d7a17da3498ee39a84f1b10300
RENATA_PRO <renatakov1010@gmail.com>
2023-03-19 16:58:44 +0100

    5

--- cart/cart.js
+++ cart/cart.js
@@ -1,2 +1,45 @@
+// // const mongoose = require('mongoose');
+// const Bucket = require('../Models/Bucket'); 
 
-    
+
+// const cart = {
+//     products: []
+//   };
+
+  
+//   function addProductToCart(productId, productName, productPrice) {
+//     const existingProductIndex = cart.products.findIndex(product => product.id === productId);
+//     if (existingProductIndex !== -1) {
+//       cart.products[existingProductIndex].quantity++;
+//     } else {
+//       cart.products.push({
+//         id: productId,
+//         name: productName,
+//         price: productPrice,
+//         quantity: 1
+//       });
+//     }
+//   }
+  
+//   function removeProductFromCart(productId) {
+//     const productIndex = cart.products.findIndex(product => product.id === productId);
+//     if (productIndex !== -1) {
+//       cart.products.splice(productIndex, 1);
+//     }
+//   }
+
+  
+//   addProductToCart('123', 'Product A', 9.99);
+
+
+//   removeProductFromCart('123');
+
+
+//   const bucket = new Bucket({
+//     products: cart.products.map(product => ({
+//       name: product.name,
+//       price: product.price
+//     }))
+//   });
+//   bucket.save();
+  
